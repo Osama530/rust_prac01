a9568d0d0b45da618eef279ea1221dc6d1f51311
Osama53 <osamaqamar93@gmail.com>
2019-09-25 23:53:33 -0700

    added main.rs

---
+++ main.rs
@@ -0,0 +1,219 @@
+fn main() {
+    // let sum = 5+6;
+    // let diff = 5-6;
+    // let pro = 5*6;
+    // let quotient = 56.7 / 32.2;
+    // let remainder = 43 % 5;
+    // let c = 'z';
+    // println!("the sum is {}",sum);
+    // println!("the diff is {}",diff);
+    // println!("the pro is {}",pro);
+    // println!("the division is {}",quotient);
+    // println!("the remainder is {}",remainder);
+    // println!("the character is {}",c);
+
+// Q1
+// let a = 5; //interger variabl 1
+// let b = 5;  // integer variable 2
+// let result;
+// if a == b {     // if two variable are same return triple of thir sum
+//     result = (a+b)*3;
+//     println!("result is {}",result);
+// }
+// else {  // else return sum of two
+//     result = a+b;
+//     println!("result is {}",result);
+// }
+
+// if satatment in let
+// let condition = false ;
+// let number = if condition {
+//     5
+// }
+// else {
+//     6
+// };
+// println!("the number is {}",number);
+
+// looping with loop command
+// let mut counter = 0;
+// let result = loop {
+//     counter += 1;
+//     if counter == 10 {
+//         break
+//         counter * 2;
+
+//     };
+// };
+// println!("result is {}",result)
+
+// // array and while loop
+// let a = [10,20,30,40,50] ;
+// let mut index = 0;
+// while index < 5 {
+//     println!("the number at index {} is {} ",index ,a[index]);
+//     index += 1;
+// }
+
+// let a = [10,20,30,40,50];
+// for element in a.iter() {
+//     println!("the value is {}", element);
+// }
+
+// let a = [10,20,30,40,50];
+// for a in 0..4 {
+//     println!("the value is {}", a);
+// }
+
+
+// let temp_f = 55.0;
+// let temp_c = (temp_f-32)*1.8;
+// println!("the temprature in celcius is {}",temp_c);
+
+// let radius = 6.0;
+// let _perimeter:i32;
+// let perimeter = 2.0*3.14*radius;
+// println!("{}",perimeter);
+
+// //short circuiting boolean logic:
+// println!("true AND false is {}",true && false);
+// println!("true OR false is {}",true || false);
+// println!("NOT true is {}",!true);
+
+// let interest: f32 = 8.0;
+// println!("{}",interest);
+
+////scope :
+// let long =1;
+// {
+//     let short =2;
+//     println!("inner short {}", short);
+//     println!("iner short {}",long);
+
+// }
+
+// println!("outer short {}",long);
+
+let a;
+{
+    let x =2;
+    a = x*x;
+
+}
+
+println!("a =  {}",a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
